3f5370524681a52158ac569ae258e4c9986245ea
yrouranos <rousseau.yannick@ouranos.ca>
2021-12-08 21:11:15 -0500

    Renamed a variable.

--- dash.py
+++ dash.py
@@ -52,55 +52,55 @@ def refresh():
 
     # Projects.
     cntx.projects = project_def.Projects(cntx=cntx)
-    projects = st.sidebar.selectbox("Choisir le projet", options=cntx.projects.get_desc_l())
-    cntx.project = project_def.Project(code=projects, cntx=cntx)
+    project_f = st.sidebar.selectbox("Choisir le projet", options=cntx.projects.get_desc_l())
+    cntx.project = project_def.Project(code=project_f, cntx=cntx)
 
     # Views.
     cntx.views = view_def.Views(cntx)
-    views = st.sidebar.radio("Choisir la vue", cntx.views.get_desc_l())
-    cntx.view = view_def.View(cntx.views.get_code(views))
+    view_f = st.sidebar.radio("Choisir la vue", cntx.views.get_desc_l())
+    cntx.view = view_def.View(cntx.views.get_code(view_f))
 
     # Plotting libraries.
     cntx.libs = lib_def.Libs(cntx.view.get_code())
-    libs = st.sidebar.radio("Choisir la librairie visuelle", options=cntx.libs.get_desc_l())
-    cntx.lib = lib_def.Lib(cntx.libs.get_code(libs))
+    lib_f = st.sidebar.radio("Choisir la librairie visuelle", options=cntx.libs.get_desc_l())
+    cntx.lib = lib_def.Lib(cntx.libs.get_code(lib_f))
 
     # Deltas.
     if cntx.view.get_code() in [view_def.mode_ts, view_def.mode_tbl, view_def.mode_map]:
         st.sidebar.markdown("<style>.sel_title {font-size:14.5px}</style>", unsafe_allow_html=True)
         st.sidebar.markdown("<p class='sel_title'>Afficher les anomalies</p>", unsafe_allow_html=True)
-        deltas = st.sidebar.checkbox("", value=False)
-        cntx.delta = deltas
+        delta_f = st.sidebar.checkbox("", value=False)
+        cntx.delta = delta_f
 
     # Variables and indices.
     cntx.varidxs = vi.VarIdxs(cntx)
-    varidxs = st.selectbox("Variable ou indice", options=cntx.varidxs.get_desc_l())
-    cntx.varidx = vi.VarIdx(cntx.varidxs.get_code(varidxs))
+    varidx_f = st.selectbox("Variable ou indice", options=cntx.varidxs.get_desc_l())
+    cntx.varidx = vi.VarIdx(cntx.varidxs.get_code(varidx_f))
     cntx.project.set_quantiles(cntx.project.get_code(), cntx)
 
     # Horizons.
     if cntx.view.get_code() in [view_def.mode_tbl, view_def.mode_map, view_def.mode_disp]:
         cntx.hors = hor_def.Hors(cntx)
-        hors = st.selectbox("Horizon", options=cntx.hors.get_code_l())
-        cntx.hor = hor_def.Hor(hors)
+        hor_f = st.selectbox("Horizon", options=cntx.hors.get_code_l())
+        cntx.hor = hor_def.Hor(hor_f)
         
     # Emission scenarios.
     cntx.rcps = rcp_def.RCPs(cntx)
     if cntx.view.get_code() in [view_def.mode_map, view_def.mode_disp]:
-        rcps = st.selectbox("Scénario d'émissions", options=cntx.rcps.get_desc_l())
-        cntx.rcp = rcp_def.RCP(cntx.rcps.get_code(rcps))
+        rcp_f = st.selectbox("Scénario d'émissions", options=cntx.rcps.get_desc_l())
+        cntx.rcp = rcp_def.RCP(cntx.rcps.get_code(rcp_f))
 
     # Statistics.
     if cntx.view.get_code() == view_def.mode_map:
         cntx.stats = stat_def.Stats(cntx)
-        stats = st.selectbox("Statistique", options=cntx.stats.get_desc_l())
-        cntx.stat = stat_def.Stat(cntx.stats.get_code(stats))
+        stat_f = st.selectbox("Statistique", options=cntx.stats.get_desc_l())
+        cntx.stat = stat_def.Stat(cntx.stats.get_code(stat_f))
 
     # Models.
     if cntx.view.get_code() == view_def.mode_disp:
         cntx.models = model_def.Models(cntx)
-        models = st.selectbox("Modèle", options=cntx.models.get_desc_l())
-        cntx.model = model_def.Model(cntx.models.get_code(models))
+        model_f = st.selectbox("Modèle", options=cntx.models.get_desc_l())
+        cntx.model = model_def.Model(cntx.models.get_code(model_f))
 
     # Components.
     if cntx.view.get_code() == view_def.mode_ts:
